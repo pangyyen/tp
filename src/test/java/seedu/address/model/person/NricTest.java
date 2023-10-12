e85a5eada3cf6276e87dce9de87eaeea334235d7
Tan Boon Khong <tanboonkhong246@gmail.com>
2023-10-12 12:53:44 +0800

    Create NricTest.java

---
+++ src/test/java/seedu/address/model/person/NricTest.java
@@ -0,0 +1,24 @@
+package seedu.address.model.person;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NricTest {
+
+    @Test
+    void isValidNric() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+}
