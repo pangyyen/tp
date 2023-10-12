906f725c141194f8e3f9ee54d934345543fa6f4d
LiYuan <lywong113@gmail.com>
2023-10-12 12:52:42 +0800

    Add tests

---
+++ src/test/java/seedu/address/model/patient/PatientTest.java
@@ -0,0 +1,40 @@
+package seedu.address.model.patient;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PatientTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getPhone() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void getAddress() {
+    }
+
+    @Test
+    void getTags() {
+    }
+
+    @Test
+    void isSamePerson() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
