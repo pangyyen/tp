91d0e41bc7905e39a929800bf9c222213d9ba4ae
Tan Boon Khong <tanboonkhong246@gmail.com>
2023-10-16 21:31:28 +0800

    Create ClinicStorage.java

---
+++ src/main/java/seedu/cc/storage/ClinicStorage.java
@@ -0,0 +1,32 @@
+package seedu.cc.storage;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
+
+import seedu.cc.commons.exceptions.DataLoadingException;
+import seedu.cc.model.ReadOnlyAddressBook;
+import seedu.cc.model.ReadOnlyUserPrefs;
+import seedu.cc.model.UserPrefs;
+
+/**
+ * API of the Storage component
+ */
+public interface ClinicStorage extends AddressBookStorage, UserPrefsStorage {
+
+    @Override
+    Optional<UserPrefs> readUserPrefs() throws DataLoadingException;
+
+    @Override
+    void saveUserPrefs(ReadOnlyUserPrefs userPrefs) throws IOException;
+
+    @Override
+    Path getAddressBookFilePath();
+
+    @Override
+    Optional<ReadOnlyAddressBook> readAddressBook() throws DataLoadingException;
+
+    @Override
+    void saveAddressBook(ReadOnlyAddressBook addressBook) throws IOException;
+
+}
