9acc34396ef5960589c98c798baca6e449254e33
pangyen <e0959104@u.nus.edu>
2023-10-17 01:04:31 +0800

    Configure FindCommandParser.java to FindPatientCommandPatser.java

---
+++ src/main/java/seedu/cc/logic/parser/FindPatientCommandParser.java
@@ -0,0 +1,33 @@
+package seedu.cc.logic.parser;
+
+import static seedu.cc.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import java.util.Arrays;
+
+import seedu.cc.logic.commands.FindPatientCommand;
+import seedu.cc.logic.parser.exceptions.ParseException;
+import seedu.cc.model.person.NameContainsKeywordsPredicate;
+
+/**
+ * Parses input arguments and creates a new FindCommand object
+ */
+public class FindPatientCommandParser implements Parser<FindPatientCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the FindCommand
+     * and returns a FindCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public FindPatientCommand parse(String args) throws ParseException {
+        String trimmedArgs = args.trim();
+        if (trimmedArgs.isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindPatientCommand.MESSAGE_USAGE));
+        }
+
+        String[] nameKeywords = trimmedArgs.split("\\s+");
+
+        return new FindPatientCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+    }
+
+}
