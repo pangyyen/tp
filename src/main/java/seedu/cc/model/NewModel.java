7ca13d73776f60bfa3a10e456ba20a7f13a203f3
L1MJH <37625953+LimJH2002@users.noreply.github.com>
2023-10-16 20:43:16 +0800

    Update NewModel.java

--- src/main/java/seedu/cc/model/NewModel.java
+++ src/main/java/seedu/cc/model/NewModel.java
@@ -5,6 +5,7 @@ import java.util.function.Predicate;
 
 import javafx.collections.ObservableList;
 import seedu.cc.commons.core.GuiSettings;
+import seedu.cc.model.patient.Patient;
 import seedu.cc.model.person.Person;
 
 /**
@@ -12,7 +13,7 @@ import seedu.cc.model.person.Person;
  */
 public interface NewModel {
     /** {@code Predicate} that always evaluate to true */
-    Predicate<Person> PREDICATE_SHOW_ALL_PERSONS = unused -> true;
+    Predicate<Patient> PREDICATE_SHOW_ALL_PERSONS = unused -> true;
 
     /**
      * Replaces user prefs data with the data in {@code userPrefs}.
@@ -53,35 +54,35 @@ public interface NewModel {
     ReadOnlyClinicBook getClinicBook();
 
     /**
-     * Returns true if a person with the same identity as {@code person} exists in the address book.
+     * Returns true if a patient with the same identity as {@code patient} exists in the clinic book.
      */
-    boolean hasPerson(Person person);
+    boolean hasPatient(Patient patient);
 
     /**
-     * Deletes the given person.
-     * The person must exist in the address book.
+     * Deletes the given patient.
+     * The patient must exist in the clinic book.
      */
-    void deletePerson(Person target);
+    void deletePatient(Patient target);
 
     /**
-     * Adds the given person.
-     * {@code person} must not already exist in the address book.
+     * Adds the given patient.
+     * {@code patient} must not already exist in the clinic book.
      */
-    void addPerson(Person person);
+    void addPatient(Patient patient);
 
     /**
-     * Replaces the given person {@code target} with {@code editedPerson}.
-     * {@code target} must exist in the address book.
-     * The person identity of {@code editedPerson} must not be the same as another existing person in the address book.
+     * Replaces the given patient {@code target} with {@code editedPatient}.
+     * {@code target} must exist in the clinic book.
+     * The patient identity of {@code editedPatient} must not be the same as another existing patient in the clinic book.
      */
-    void setPerson(Person target, Person editedPerson);
+    void setPatient(Patient target, Patient editedPatient);
 
-    /** Returns an unmodifiable view of the filtered person list */
-    ObservableList<Person> getFilteredPersonList();
+    /** Returns an unmodifiable view of the filtered patient list */
+    ObservableList<Patient> getFilteredPatientList();
 
     /**
-     * Updates the filter of the filtered person list to filter by the given {@code predicate}.
+     * Updates the filter of the filtered patient list to filter by the given {@code predicate}.
      * @throws NullPointerException if {@code predicate} is null.
      */
-    void updateFilteredPersonList(Predicate<Person> predicate);
+    void updateFilteredPatientList(Predicate<Patient> predicate);
 }
