296a4868be96e9089db64ad1d51f98d58da3a2d7
L1MJH <37625953+LimJH2002@users.noreply.github.com>
2023-10-26 22:19:16 +0800

    Create Tabs.java

---
+++ src/main/java/seedu/cc/commons/core/tabs/Tabs.java
@@ -0,0 +1,22 @@
+package seedu.cc.commons.core.tabs;
+
+public enum Tabs {
+    PATIENTS("Patients"),
+    MEDICAL_HISTORY("Medical History"),
+    APPOINTMENTS("Appointments");
+
+    private final String displayName;
+
+    Tabs(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName;
+    }
+}
