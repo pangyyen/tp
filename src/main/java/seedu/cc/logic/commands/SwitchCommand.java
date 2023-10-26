753eb6837b8fd8b0b1369539dabb2f11e6b8c772
L1MJH <37625953+LimJH2002@users.noreply.github.com>
2023-10-26 23:45:39 +0800

    Enhance switch command

--- src/main/java/seedu/cc/logic/commands/SwitchCommand.java
+++ src/main/java/seedu/cc/logic/commands/SwitchCommand.java
@@ -3,13 +3,14 @@ package seedu.cc.logic.commands;
 import static java.util.Objects.requireNonNull;
 
 import seedu.cc.commons.core.index.Index;
+import seedu.cc.commons.core.tabs.Tabs;
 import seedu.cc.logic.commands.exceptions.CommandException;
 import seedu.cc.model.Model;
 
 public class SwitchCommand  extends Command {
 
     public static final String COMMAND_WORD = "switch";
-    public static final String MESSAGE_SUCCESS = "Switched to";
+    public static final String MESSAGE_SUCCESS = "Switched to ";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Switches to the tab specified by the index number "
             + "used in the navigating sidebar.\n"
@@ -25,6 +26,19 @@ public class SwitchCommand  extends Command {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         model.setCurrentTab(this.tabIndex.getZeroBased());
-        return new CommandResult(String.format(MESSAGE_SUCCESS, tabIndex.getOneBased() + " tab"));
+        return new CommandResult(String.format(MESSAGE_SUCCESS + "%s tab", this.getTabName(this.tabIndex)));
+    }
+
+    private Tabs getTabName(Index tabIndex) {
+        switch (tabIndex.getOneBased()) {
+        case 1:
+            return Tabs.PATIENTS;
+        case 2:
+            return Tabs.MEDICAL_HISTORY;
+        case 3:
+            return Tabs.APPOINTMENTS;
+        default:
+            return null;
+        }
     }
 }
