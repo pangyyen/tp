6bd2db03d6179b099dd7a93bc935d16fd8333644
L1MJH <37625953+LimJH2002@users.noreply.github.com>
2023-10-26 16:01:26 +0800

    Remove menu bar

--- src/main/java/seedu/cc/ui/MainWindow.java
+++ src/main/java/seedu/cc/ui/MainWindow.java
@@ -40,8 +40,8 @@ public class MainWindow extends UiPart<Stage> {
     @FXML
     private StackPane commandBoxPlaceholder;
 
-    @FXML
-    private MenuItem helpMenuItem;
+//    @FXML
+//    private MenuItem helpMenuItem;
 
     @FXML
     private StackPane personListPanelPlaceholder;
@@ -71,7 +71,7 @@ public class MainWindow extends UiPart<Stage> {
         // Configure the UI
         setWindowDefaultSize(logic.getGuiSettings());
 
-        setAccelerators();
+//        setAccelerators();
 
         helpWindow = new HelpWindow();
     }
@@ -80,9 +80,9 @@ public class MainWindow extends UiPart<Stage> {
         return primaryStage;
     }
 
-    private void setAccelerators() {
-        setAccelerator(helpMenuItem, KeyCombination.valueOf("F1"));
-    }
+//    private void setAccelerators() {
+//        setAccelerator(helpMenuItem, KeyCombination.valueOf("F1"));
+//    }
 
     /**
      * Sets the accelerator of a MenuItem.
