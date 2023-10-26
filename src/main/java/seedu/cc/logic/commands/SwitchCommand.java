8cfd607ac974c37668e8ea19f52df3de7ec6ad62
L1MJH <37625953+LimJH2002@users.noreply.github.com>
2023-10-26 22:03:43 +0800

    Update SwitchCommand.java

--- src/main/java/seedu/cc/logic/commands/SwitchCommand.java
+++ src/main/java/seedu/cc/logic/commands/SwitchCommand.java
@@ -1,14 +1,25 @@
 package seedu.cc.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
+import seedu.cc.commons.core.index.Index;
 import seedu.cc.logic.commands.exceptions.CommandException;
 import seedu.cc.model.Model;
+import seedu.cc.model.medicalhistory.MedicalHistoryEvent;
 
 public class SwitchCommand  extends Command {
 
     public static final String COMMAND_WORD = "switch";
-    public static final String MESSAGE_SUCCESS = "Switched to %d tab!";
+    public static final String MESSAGE_SUCCESS = "Switched to";
+
+    private final Index tabIndex;
+
+    public SwitchCommand(Index tabIndex) {
+        requireNonNull(tabIndex);
+        this.tabIndex = tabIndex;
+    }
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        return null;
+        return new CommandResult(String.format(MESSAGE_SUCCESS, tabIndex.getOneBased() + " tab"));
     }
 }
