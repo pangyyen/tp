0bd4dc12e366b6380c5c32bd39f875605f899db0
LiYuan <lywong113@gmail.com>
2023-10-31 22:40:25 +0800

    Fix checkstyle

--- src/main/java/seedu/cc/logic/parser/medicalhistory/EditMedicalHistoryEventCommandParser.java
+++ src/main/java/seedu/cc/logic/parser/medicalhistory/EditMedicalHistoryEventCommandParser.java
@@ -1,7 +1,6 @@
 package seedu.cc.logic.parser.medicalhistory;
 
 import static java.util.Objects.requireNonNull;
-
 import static seedu.cc.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.cc.logic.parser.CliSyntax.PREFIX_DATE;
 import static seedu.cc.logic.parser.CliSyntax.PREFIX_MEDICAL_CONDITION;
