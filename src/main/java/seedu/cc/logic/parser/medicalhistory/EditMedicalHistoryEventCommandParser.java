b57425be715b215677d0278d59ee973007086a33
LiYuan <lywong113@gmail.com>
2023-11-13 23:09:43 +0800

    Fix bug where no date in edit medical history

--- src/main/java/seedu/cc/logic/parser/medicalhistory/EditMedicalHistoryEventCommandParser.java
+++ src/main/java/seedu/cc/logic/parser/medicalhistory/EditMedicalHistoryEventCommandParser.java
@@ -73,12 +73,11 @@ public class EditMedicalHistoryEventCommandParser implements Parser<EditMedicalH
         }
 
         if (argMultimap.getValue(PREFIX_DATE).isPresent()) {
+            if (ParserUtil.parseDate(argMultimap.getValue(PREFIX_DATE).get()).isFutureDate()) {
+                throw new ParseException(String.format(EditMedicalHistoryEventCommand.MESSAGE_DATE_IN_FUTURE));
+            }
             editMedicalHistoryEventDescriptor.setDate(ParserUtil.parseDate(argMultimap
                     .getValue(PREFIX_DATE).get()));
-        }
-
-        if (ParserUtil.parseDate(argMultimap.getValue(PREFIX_DATE).get()).isFutureDate()) {
-            throw new ParseException(String.format(EditMedicalHistoryEventCommand.MESSAGE_DATE_IN_FUTURE));
         }
 
         return new EditMedicalHistoryEventCommand(eventIndex, patientIndex, editMedicalHistoryEventDescriptor);
